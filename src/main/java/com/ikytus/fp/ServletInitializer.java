86d527a8db322ea017d5246ea7ca9bc37deb2c0e
franzecs@gmail.com <franzecs@gmail.com>
2018-07-19 12:32:13 -0300

    primeiro commit

---
+++ src/main/java/com/ikytus/fp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ikytus.fp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WorkshopmongoApplication.class);
+	}
+
+}
